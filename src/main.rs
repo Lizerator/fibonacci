890191281b2988f35abf55393558bd050650471e
Lizerator <lizorlizerator@gmail.com>
2023-09-19 17:46:20 -0700

    Fixed Project

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,10 @@ fn main() {
         println!("Which fibonacci number are you looking for?");
         io::stdin().read_line(&mut n).expect("Failed to read line!");
 
-        let answer: i32 = match n.trim().parse() {
-            Ok(num) => match num < 0 {
-                true => {
-                    println!("Please input a number greater than zero!");
-                    continue;
-                }
-                false => fibonacci(num),
-            },
+        let answer: u32 = match n.trim().parse() {
+            Ok(num) => fibonacci(num),
             Err(_) => {
-                println!("Please input a number!");
+                println!("Please input a number greater than or equal to 0!");
                 continue;
             }
         };
@@ -24,7 +18,7 @@ fn main() {
     }
 }
 
-fn fibonacci(num: i32) -> i32 {
+fn fibonacci(num: u32) -> u32 {
     match num {
         0 => 0,
         1 => 1,
